971db8183047c466dd53f8ec461ae4900baf23da
krishnadesh4466 <147423976+krishnadesh4466@users.noreply.github.com>
2023-10-09 22:55:24 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,23 @@
+import random
+rannum=random.randint(1,50)
+gueesed=None
+guess=0
+while(rannum!=gueesed):
+    gueesed=int(input("guess a number:"))
+    if(rannum==gueesed):
+        print("number is right u gueesed right")
+        guess+=1
+    else:
+        print("you gueesed wrong number try again!")
+        guess=+1
+    if(rannum>gueesed):
+        print("the number is small")
+    elif(rannum<gueesed):
+        print("the number is large")
+with open("hiscore.txt","r") as f:
+    hiscore= int(f.read())
+if(hiscore>guess):
+    with open("hiscore.txt","w")as f:
+        hiscore=f.write(str(guess))
+    
+
